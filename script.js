4d130a1b3f5383a7ef89efb993055f4bb260b3f7
sougata <sougataghar47@gmail.com>
2025-01-11 13:22:54 +0530

    first push

---
+++ script.js
@@ -0,0 +1,91 @@
+ 
+
+// Register the ScrollTrigger plugin
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+   
+  spaceBetween: 100,
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true, // Makes the dots clickable
+},
+
+});
+const mediaQuery = window.matchMedia("(min-width: 1024px)");
+
+if (mediaQuery.matches) {
+  gsap.registerPlugin(ScrollTrigger);
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 100,
+    autoplay:false,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Makes the dots clickable
+  },
+ 
+  });
+  Array.from(document.querySelector('.hero').children).forEach((element,index) => {
+    gsap.from(element, { y: 50,opacity:0, duration: .5,delay:.5*index });
+    
+  });
+
+  // Target all children of the ".about" section and apply animation
+  [...document.querySelector(".about").children].forEach((element, index) => {
+    gsap.from(element, {
+      x: 50,
+      opacity: 0,
+      duration: 0.5,
+      delay: 0.5 * index,
+      scrollTrigger: {
+        trigger: element,  
+        start: "top 80%",  
+        end: "top 30%",   
+    },
+    });
+  });
+  [...document.querySelector(".services").firstElementChild.children].forEach((element,index) => {
+    gsap.from(element, {
+      y: 50,
+      opacity: 0,
+      duration: 0.5,
+      delay: 0.25 * index,
+      scrollTrigger: {
+        trigger: element,  
+        start: "top 80%",  
+        end: "top 20%",   
+   
+      },
+    });
+  });
+  [...document.querySelectorAll(".staff")].forEach((element,index)=>{
+    gsap.from(element, {
+      y: 50,
+      opacity: 0,
+      duration: 0.5,
+      delay: 0.25 * index,
+      scrollTrigger: {
+        trigger: element,  
+        start: "top 80%",  
+        end: "top 20%",   
+   
+      },
+    });
+  });
+  [...document.querySelectorAll(".blog")].forEach((element,index)=>{
+    gsap.from(element, {
+      y: -50,
+      opacity: 0,
+      duration: 0.2,
+      delay: 0.25 * index,
+      scrollTrigger: {
+        trigger: element,  
+        start: "top 80%",  
+        end: "top 20%",   
+   
+      },
+    });
+  })
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Include the HTML file in the root directory
+    "./src/**/*.{html,js,jsx}", // Include files inside the `src` folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
